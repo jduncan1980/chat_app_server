3d2746edf8b6410790e866d5d5170f5cab8d5cc5
Jason Duncan <jasonduncan@jasons-mbp.myfiosgateway.com>
2020-11-28 22:15:01 -0500

    added socket.io

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const http = require('http');
+const socket = require('socket.io');
 const logger = require('./middleware/logger');
 const handleError = require('./middleware/handle-error-middleware');
 const restricted = require('./middleware/restricted-route-middleware');
 const authRouter = require('./api/auth/auth-router');
 
-const server = express();
-server.use(cors()).use(helmet()).use(logger()).use(express.json());
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 
-// server.use('/api/users', restricted, userRouter);
-server.use('/api/auth', authRouter);
+io.on('connection', (server) => {
+	console.log('New WS Connection.');
+	socket.emit('connection', null);
+});
+app.use(cors()).use(helmet()).use(logger()).use(express.json());
 
-server.use(handleError());
-server.get('/', (req, res) => {
+// app.use('/api/users', restricted, userRouter);
+app.use('/api/auth', authRouter);
+
+app.use(handleError());
+app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Server is Running.' });
 });
 
